dd10636372f6d8cc31bbe7d1b1f7e87989df4373
seyeon <tltptltptltp@gmail.com>
2020-11-24 23:41:29 +0900

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT=4000;
+
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+
+
+const handleHome =(req,res) => res.send("hello");
+
+app.get("/",handleHome);
+
+app.listen(PORT, handleListening);
