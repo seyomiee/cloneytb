36234e9beef339a5a0c7c4592fd324c11f38cb8d
seyeon <tltptltptltp@gmail.com>
2020-12-03 17:50:52 +0900

    done

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,2 +1,13 @@
-export const home= (req,res) => res.send("home");
-export const search= (req,res) => res.send("search");
+export const home= (req,res) => res.render("home", {pageTitle: "home"});
+
+export const search= (req,res) =>{
+    const { query : { term : searchingBy }} =req;
+    res.render("search", {pageTitle: "search", searchingBy});
+};
+
+export const upload = (req, res) => res.render("upload", {pageTitle : "upload"});
+
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle : "videoDetail"});
+
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle : "editVideo"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle : "deleteVideo"});
